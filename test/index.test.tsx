6fee2c1000b30b559961edcd588609ad373a9be6
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-03-18 07:54:05 +0800

    chore: update test files

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src/index';
+import { LionPlayer } from '../src/index';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<LionPlayer />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
