d55c592406ded159658d388fdf8a70e2d4af40b5
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-03-18 09:19:21 +0800

    chore: add MPD_SOURCE in constants

--- src/constants/sources.ts
+++ src/constants/sources.ts
@@ -6,6 +6,12 @@ export const M3U8_SOURCE: VIDEOJS.Tech.SourceObject = {
   type: 'application/x-mpegURL',
 };
 
+export const MPD_SOURCE: VIDEOJS.Tech.SourceObject = {
+  src: 'https://vod.lyon.com.ph/file/lyon-vod/processed/bb51a51f70da12c3c2e2d81fbaf83485/h264_master.mpd',
+  type: 'application/dash+xml',
+};
+
 export const SOURCES: VIDEOJS.Tech.SourceObject[] = [
+  MPD_SOURCE,
   M3U8_SOURCE,
 ]
