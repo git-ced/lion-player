826749a579f55fa0dc03420bc0ba56751636a667
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-03-18 09:21:37 +0800

    chore: update test cases

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { LionPlayer } from '../src/index';
+import { M3U8_SOURCE, MPD_SOURCE } from '../src/constants/sources';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<LionPlayer />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  it('mpd renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<LionPlayer sources={[MPD_SOURCE]} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  it('m3u8 renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<LionPlayer sources={[M3U8_SOURCE]} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
