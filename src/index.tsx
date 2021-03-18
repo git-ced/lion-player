463c78eec1e06d29bede5b4fe9769585ce19a9ee
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-03-18 09:50:24 +0800

    fix: remove unnecessary container styles

--- src/index.tsx
+++ src/index.tsx
@@ -11,20 +11,18 @@ export const LionPlayer = (props?: VideoJsPlayerOptions) => {
   const playerRef = usePlayer({ ...props });
 
   return (
-    <div style={{ width: '50%', height: 'auto' }}>
-      <div data-vjs-player>
-        <video
-          ref={playerRef}
-          className="video-js vjs-big-play-centered"
-          data-setup='{}'
-        >
-          <p className="vjs-no-js">
-            To view this video please enable JavaScript, and consider upgrading to a
-            web browser that <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video
-            </a>
-          </p>
-        </video>
-      </div>
+    <div data-vjs-player>
+      <video
+        ref={playerRef}
+        className="video-js vjs-big-play-centered"
+        data-setup='{}'
+      >
+        <p className="vjs-no-js">
+          To view this video please enable JavaScript, and consider upgrading to a
+          web browser that <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video
+          </a>
+        </p>
+      </video>
     </div>
   )
 };
