787ac816a8e035c9f017a066bcf4cb4c75a0385a
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-03-18 13:19:03 +0800

    feat: orient volume slider vertically

--- src/constants/options.ts
+++ src/constants/options.ts
@@ -3,13 +3,18 @@ import { VideoJsPlayerOptions } from 'video.js'
 
 export const DEFAULT_OPTIONS: VideoJsPlayerOptions = {
   controls: true,
+  controlBar: {
+    volumePanel: {
+      inline: false
+    }
+  },
   autoplay: false,
   fluid: true,
   preload: 'auto',
   width: 100,
-  playbackRates: [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2],
+  playbackRates: [0.25, 0.5, 0.75, 1, 1.25, 1.5, 2],
   html5: {
-    hls: {
+    vhs: {
       enableLowInitialPlaylist: true,
       smoothQualityChange: true,
       overrideNative: true,
