711599881306097b2335a7d6c9f1e685d293e22b
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-05-24 09:08:30 +0800

    refactor: use uncontrolled component in the controlled component

--- src/index.tsx
+++ src/index.tsx
@@ -9,13 +9,15 @@ import './themes/lion.css';
 // ANCHOR Hooks
 import { usePlayer } from './hooks/usePlayer';
 
-export const LionPlayer = (props?: VideoJsPlayerOptions) => {
-  const playerRef = usePlayer({ ...props });
+export interface IUncontrolledPlayerProps {
+  player: React.LegacyRef<HTMLVideoElement>
+}
 
+export const UncontrolledLionPlayer = ({ player }: IUncontrolledPlayerProps) => {
   return (
     <div data-vjs-player>
       <video
-        ref={playerRef}
+        ref={player}
         className="video-js vjs-theme-lion"
       >
         <p className="vjs-no-js">
@@ -28,23 +30,8 @@ export const LionPlayer = (props?: VideoJsPlayerOptions) => {
   )
 };
 
-export interface IUncontrolledPlayerProps {
-  ref: React.LegacyRef<HTMLVideoElement>
-}
+export const LionPlayer = (props?: VideoJsPlayerOptions) => {
+  const playerRef = usePlayer({ ...props });
 
-export const UncontrolledLionPlayer = ({ ref }: IUncontrolledPlayerProps) => {
-  return (
-    <div data-vjs-player>
-      <video
-        ref={ref}
-        className="video-js vjs-theme-lion"
-      >
-        <p className="vjs-no-js">
-          To view this video please enable JavaScript, and consider upgrading to a
-          web browser that <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video
-          </a>
-        </p>
-      </video>
-    </div>
-  )
+  return <UncontrolledLionPlayer player={playerRef} />;
 };
