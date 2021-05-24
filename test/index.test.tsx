23b7f1032a1fd909e8f400ce742a690f6374e8c2
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-05-24 09:09:16 +0800

    chore: add test case for the uncontrolled Lion Player

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { LionPlayer } from '../src/index';
+import { LionPlayer, UncontrolledLionPlayer } from '../src/index';
 import { M3U8_SOURCE, MPD_SOURCE } from '../src/constants/sources';
 
-describe('it', () => {
+describe('render controlled Lion Player', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<LionPlayer />, div);
@@ -17,6 +17,16 @@ describe('it', () => {
   it('m3u8 renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<LionPlayer sources={[M3U8_SOURCE]} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
+
+describe('render uncontrolled Lion Player', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    const playerRef = React.createRef<HTMLVideoElement>();
+
+    ReactDOM.render(<UncontrolledLionPlayer player={playerRef} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
